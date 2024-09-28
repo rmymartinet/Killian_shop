d35fd04246ebc8f4f06656a5d3943bf8419ce67c
martinet <remymartinet.contact@gmail.com>
2024-09-28 11:47:17 +0200

    Add Card component for Eco.jsx

---
+++ src/app/_components/Card.tsx
@@ -0,0 +1,17 @@
+interface CardProps {
+  title: string;
+  description: string;
+}
+
+const Card = ({ title, description }: CardProps) => {
+  return (
+    <div className="rounded-xl shadow-xl p-10 flex gap-8 w-full">
+      <div className="flex flex-col gap-4">
+        <h4 className="font-semibold">{title}</h4>
+        <p className="text-pretty">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
