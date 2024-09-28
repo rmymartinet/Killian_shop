0c7b226599ed15367c2a3c4dc20c5fd3d17ffafe
martinet <remymartinet.contact@gmail.com>
2024-09-28 14:13:24 +0200

    Fix type Error in interface

--- src/app/_components/ProductCard.tsx
+++ src/app/_components/ProductCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import TransitionLink from "./TransitionLinks";
 
 interface ProductCardProps {
-  imageSrc?: string;
+  imageSrc: string; // Ce doit être une chaîne, pas un tableau
   id: string;
   price: string;
   title: string;
@@ -21,14 +21,14 @@ const ProductCard = ({
   return (
     <div className="flex flex-col justify-center items-center">
       <TransitionLink href={`/products/${id}`}>
-        <div className="flex flex-col items-center gap-20 lg:gap-40 ">
+        <div className="flex flex-col items-center gap-20 lg:gap-40">
           <div className="w-48 h-48 mr-10 lg:w-[25vw] lg:h-[25vw]">
             <Image
               className="mb-12 self-center cursor-pointer"
               width={imageWidth || 700}
               height={imageHeight || 700}
-              src={imageSrc?.[0] ?? "/path/to/default/image.jpg"}
-              alt=""
+              src={imageSrc}
+              alt={title || "Produit"}
               layout="responsive"
               objectFit="cover"
             />
