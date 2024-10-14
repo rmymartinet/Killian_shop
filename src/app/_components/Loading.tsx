ed898ab060fafd9060d4bd15009d6a3bee227f84
martinet <remymartinet.contact@gmail.com>
2024-10-14 22:53:57 +0200

    Simplified and responsive animation. Added missing H1 tags on mobile and tablet breakpoints.

--- src/app/_components/Loading.tsx
+++ src/app/_components/Loading.tsx
@@ -1,20 +1,58 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import React from "react";
+import useWindow from "../hooks/useWindow";
+
+// Constantes pour les opacités selon le breakpoint
+const OPACITIES_DESKTOP = Array.from({ length: 9 }, (_, i) =>
+  Math.max(90 - i * 10, 5)
+);
+const OPACITIES_TABLET = Array.from({ length: 20 }, (_, i) =>
+  Math.max(95 - i * 6, 5)
+);
+const OPACITIES_MOBILE = Array.from({ length: 30 }, (_, i) =>
+  Math.max(95 - i * 3, 5)
+);
 
 const LoadingPage = ({
   setIsAnimated,
 }: {
   setIsAnimated: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const h1Refs = Array.from({ length: 9 }).map(() =>
+  const { width } = useWindow();
+
+  // Fonction pour déterminer le nombre de <h1> et les opacités à afficher en fonction de la largeur de la fenêtre
+  const getHeadingData = (width: number) => {
+    if (width > 1024) {
+      return {
+        numberOfHeadings: OPACITIES_DESKTOP.length,
+        opacities: OPACITIES_DESKTOP,
+      };
+    }
+    if (width > 498 && width <= 1024) {
+      return {
+        numberOfHeadings: OPACITIES_TABLET.length,
+        opacities: OPACITIES_TABLET,
+      };
+    }
+    return {
+      numberOfHeadings: OPACITIES_MOBILE.length,
+      opacities: OPACITIES_MOBILE,
+    };
+  };
+
+  // Obtenir le nombre de <h1> et les opacités appropriées
+  const { numberOfHeadings, opacities } = getHeadingData(width);
+
+  // Créer des références pour chaque <h1>
+  const h1Refs = Array.from({ length: numberOfHeadings }).map(() =>
     React.createRef<HTMLHeadingElement>()
   );
 
   useGSAP(() => {
     h1Refs.forEach((ref, index) => {
       gsap.to(ref.current, {
-        opacity: 0.9 - index * 0.1,
+        opacity: opacities[index] / 100, // Utiliser les opacités définies en divisant par 100
         yPercent: 0,
         duration: 0.5,
         ease: "power3.inOut",
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,12 +4,24 @@ import { animatePageIn } from "@/utils/Animation";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
+import useWindow from "./hooks/useWindow";
 
 const TEXT = "sois fier de tes sapes";
-const OPACITIES = [90, 80, 70, 60, 50, 40, 30, 20, 10, 10];
+const OPACITIES_DESKTOP = Array.from({ length: 9 }, (_, i) =>
+  Math.max(90 - i * 10, 5)
+);
+
+const OPACITIES_TABLET = Array.from({ length: 20 }, (_, i) =>
+  Math.max(95 - i * 6, 5)
+);
+const OPACITIES_MOBILE = Array.from({ length: 30 }, (_, i) =>
+  Math.max(95 - i * 3, 5)
+);
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+
+  const { width } = useWindow();
 
   useEffect(() => {
     // Lancer l'animation lors du changement de route
@@ -21,19 +33,43 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <div className="overflow-hidden">
         <div
           id="sentence"
-          className="min-h-screen w-screen bg-white z-10 fixed top-0 left-0"
+          className="min-h-screen w-screen flex flex-col bg-white z-10 fixed top-0 left-0"
           style={{ lineHeight: "1" }}
         >
-          {OPACITIES.map((opacity, index) => (
-            <h1
-              key={index}
-              id={`text-${index}`}
-              className="text-black lg:text-[8vw] fixed top-0 left-0 uppercase font-medium z-50"
-              style={{ opacity: opacity / 100 }}
-            >
-              {TEXT}
-            </h1>
-          ))}
+          {width > 1024 &&
+            OPACITIES_DESKTOP.map((opacity, index) => (
+              <h1
+                key={index}
+                id={`text-${index}`}
+                className="text-black text-[8vw] uppercase font-medium z-50"
+                style={{ opacity: opacity / 100 }}
+              >
+                {TEXT}
+              </h1>
+            ))}
+          {width > 498 &&
+            width < 1024 &&
+            OPACITIES_TABLET.map((opacity, index) => (
+              <h1
+                key={index}
+                id={`text-${index}`}
+                className="text-black text-[8vw] uppercase font-medium z-50"
+                style={{ opacity: opacity / 100 }}
+              >
+                {TEXT}
+              </h1>
+            ))}
+          {width <= 498 &&
+            OPACITIES_MOBILE.map((opacity, index) => (
+              <h1
+                key={index}
+                id={`text-${index}`}
+                className="text-black text-[8vw] uppercase font-medium z-50"
+                style={{ opacity: opacity / 100 }}
+              >
+                {TEXT}
+              </h1>
+            ))}
         </div>
         <motion.div
           initial={{ y: 20 }}
