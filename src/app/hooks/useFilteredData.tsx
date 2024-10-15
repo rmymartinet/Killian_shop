57ca35752a82ef34a4cf7c19487410aeee0d7a9d
martinet <remymartinet.contact@gmail.com>
2024-10-15 14:12:26 +0200

    Modified to conditionally display the filter argument, making it optional.

--- src/app/hooks/useFilteredData.tsx
+++ src/app/hooks/useFilteredData.tsx
@@ -2,7 +2,7 @@ import { Data } from "@/types/dataTypes";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-export function useFilteredData(filter: string) {
+export function useFilteredData(filter?: string) {
   const [data, setData] = useState<Data[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
@@ -10,11 +10,16 @@ export function useFilteredData(filter: string) {
     async function fetchData() {
       try {
         const response = await axios.get<Data[]>("/api/products");
-        const filteredData = response.data.filter(
-          (item) => item.category === filter
-        );
+        let filteredData = response.data;
+        if (filter) {
+          filteredData = response.data.filter(
+            (item) => item.category === filter
+          );
+        }
         setData(filteredData);
       } catch (error) {
+        console.log(error);
+        console.error("Error fetching items");
         setData([]);
       } finally {
         setLoading(false);
