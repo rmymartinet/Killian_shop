effce355cd74228d7ee90515d72ad3a72212a28d
rmymartinet <remymartinet.contact@gmail.com>
2024-10-22 15:28:33 +0200

    Add createRouteMatcher to middleware.ts for protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/admin"]);
 
 export default clerkMiddleware();
 
