ee0c324e047f3dce06f748a54086d2287eb6866d
rmymartinet <remymartinet.contact@gmail.com>
2024-10-22 11:43:26 +0200

    Add Access-Control-Allow-Origin header to /api routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 export default {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://sfdts.fr/",
+          },
+        ],
+      },
+    ];
+  },
   images: {
-    domains: ["res.cloudinary.com"], // Ajoute Cloudinary ici
+    domains: ["res.cloudinary.com"],
   },
 };
