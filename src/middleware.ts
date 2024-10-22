75dbe468b48be2431c7c73b413b5641d5010a7f9
rmymartinet <remymartinet.contact@gmail.com>
2024-10-22 15:37:41 +0200

    Refactor clerkMiddleware to protect routes based on createRouteMatcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-const isProtectedRoute = createRouteMatcher(["/admin"]);
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(["/admin(/.*)?"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
   matcher: [
