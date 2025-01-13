7b571069ce2a6c1afa70c57cbcd81638d769565d
rmymartinet <remymartinet.contact@gmail.com>
2025-01-14 00:49:12 +0100

    Refactor ProductCard component: replace TransitionLink with Next.js Link, simplify layout, and remove price display

--- src/app/_components/ProductCard.tsx
+++ src/app/_components/ProductCard.tsx
@@ -1,11 +1,10 @@
 import { Data } from "@/types/dataTypes";
 import Image from "next/image";
-import TransitionLink from "./TransitionLinks";
+import Link from "next/link";
 
 const ProductCard = ({
   imageUrls,
   id,
-  price,
   title,
   imageWidth,
   imageHeight,
@@ -15,38 +14,29 @@ const ProductCard = ({
 
   return (
     <div
-      className={`flex flex-col justify-center items-center border border-slate-200 p-2 ${
+      className={`flex flex-col justify-center items-center ${
         isOutOfStock ? "opacity-50 blur-[1px]" : ""
       }`}
     >
-      <TransitionLink href={`/products/${id}`}>
-        <div className="flex flex-col items-center gap-20 lg:gap-40">
-          <div className="w-48 h-48 mr-10 lg:w-[20vw] lg:h-[20vw]">
+      <Link href={`/products/${id}`}>
+        <div className="flex flex-col items-center">
+          <div className="lg:w-[25vw]">
             <Image
-              className="mb-12 self-center cursor-pointer"
+              className="self-center pr-10 w-full h-full object-cover cursor-pointer"
               width={imageWidth || 700}
               height={imageHeight || 700}
               src={imageUrls}
               alt={title || "Produit"}
             />
           </div>
-          <div className="flex items-center justify-between w-full text-sm px-2">
-            <div className="flex flex-col items-start justify-start gap-1">
-              <span className="font-semibold flex-1 text-black">{title}</span>
-              <p className="text-slate-500 flex-1 lg:text-end justify-self-end">
-                {price},00 €
-              </p>
-            </div>
-            {isOutOfStock ? (
-              <span className="text-red-500">Out of Stock</span>
-            ) : (
-              <span className="text-black w-7 h-7 rounded-full glassmorphism  grid place-content-center">
-                +
-              </span>
-            )}
-          </div>
+          <Link
+            href={`/products/${id}`}
+            className="text-sm text-black border-b border-black cursor-pointer"
+          >
+            {title}
+          </Link>
         </div>
-      </TransitionLink>
+      </Link>
     </div>
   );
 };
