4580cf493e847b5af655858ab02c571c3494eb72
rmymartinet <remymartinet.contact@gmail.com>
2025-01-14 00:48:28 +0100

    Refactor Template component: remove unused window hook and opacity logic, update animation to fade in content

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,23 +4,9 @@ import { animatePageIn } from "@/utils/Animation";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-import useWindow from "./hooks/useWindow";
-
-const TEXT = "sois fier de tes sapes";
-const OPACITIES_DESKTOP = Array.from({ length: 9 }, (_, i) =>
-  Math.max(90 - i * 10, 5)
-);
-
-const OPACITIES_TABLET = Array.from({ length: 20 }, (_, i) =>
-  Math.max(95 - i * 6, 5)
-);
-const OPACITIES_MOBILE = Array.from({ length: 30 }, (_, i) =>
-  Math.max(95 - i * 3, 5)
-);
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const { width } = useWindow();
 
   useEffect(() => {
     // Lancer l'animation lors du changement de route
@@ -30,49 +16,10 @@ export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="overflow-hidden">
-        <div
-          id="sentence"
-          className="min-h-screen w-[100dvw] flex flex-col bg-white z-10 fixed top-0 left-0"
-          style={{ lineHeight: "1" }}
-        >
-          {width > 1024 &&
-            OPACITIES_DESKTOP.map((opacity, index) => (
-              <h1
-                key={index}
-                id={`text-${index}`}
-                className="text-black text-8xl text-[8vw] uppercase font-medium z-50"
-                style={{ opacity: opacity / 100 }}
-              >
-                {TEXT}
-              </h1>
-            ))}
-          {width > 498 &&
-            width <= 1024 &&
-            OPACITIES_TABLET.map((opacity, index) => (
-              <h1
-                key={index}
-                id={`text-${index}`}
-                className="text-black text-8xl text-[8vw] uppercase font-medium z-50"
-                style={{ opacity: opacity / 100 }}
-              >
-                {TEXT}
-              </h1>
-            ))}
-          {width <= 498 &&
-            OPACITIES_MOBILE.map((opacity, index) => (
-              <h1
-                key={index}
-                id={`text-${index}`}
-                className="text-black text-8xl text-[8vw] uppercase font-medium z-50"
-                style={{ opacity: opacity / 100 }}
-              >
-                {TEXT}
-              </h1>
-            ))}
-        </div>
         <motion.div
-          initial={{ y: 20 }}
-          animate={{ y: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
           transition={{ ease: "easeInOut", duration: 0.75, delay: 0.5 }}
         >
           {children}
