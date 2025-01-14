9d0c431cd7d3bee8605e2b6cddd99ff134580cae
rmymartinet <remymartinet.contact@gmail.com>
2025-01-14 15:16:58 +0100

    Refactor NavBar component: update layout for better alignment and add logo image for branding

--- src/app/_components/Nav/NavBar.tsx
+++ src/app/_components/Nav/NavBar.tsx
@@ -8,12 +8,13 @@ import {
 import Link from "next/link";
 import { FaShoppingCart } from "react-icons/fa";
 import TransitionLink from "../TransitionLinks";
+import Image from "next/image";
 
 export default function Nav() {
   const { user, isLoaded } = useUser();
 
   return (
-    <nav className="flex fixed p-5 inset-0 items-center h-max z-50">
+    <nav className="flex justify-between fixed p-5 inset-0 items-center h-max z-50">
       <div className="bg-black text-white flex gap-10 p-5 rounded-2xl text-md font-semibold shadow-xl">
         <Link href="/">Home</Link>
         <Link href="/shop">Shop</Link>
@@ -21,7 +22,16 @@ export default function Nav() {
           <TransitionLink href="/admin" label="Admin" />
         )}
       </div>
-      <div className="absolute right-10 flex items-center gap-20 p-5">
+      <div className="h-[10vh] fixed top-5 left-1/2 -translate-x-1/2">
+        <Image
+          alt=""
+          src="/assets/logo_remove_bg.png"
+          width={400}
+          height={400}
+          className="w-full h-full object-contain"
+        />
+      </div>
+      <div className="flex items-center gap-20 p-5">
         <SignedOut>
           <SignInButton />
         </SignedOut>
